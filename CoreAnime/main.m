9c460a6da7eb863f733f158934386725ae050bd6
Amir J <amir.jahan@gmail.com>
2017-08-22 14:26:18 -0700

    Initial Commit

---
+++ CoreAnime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreAnime
+//
+//  Created by Amir on 8/22/17.
+//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
